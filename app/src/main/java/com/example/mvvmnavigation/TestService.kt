6d9f4785077233c8cd92368258a53dba2f212075
oleksandr.bortnyk <oleksandr.bortnyk@vimmi.net>
2021-05-06 21:46:13 +0300

    continue with IntentService

--- app/src/main/java/com/example/mvvmnavigation/TestService.kt
+++ app/src/main/java/com/example/mvvmnavigation/TestService.kt
@@ -7,9 +7,19 @@ import android.os.IBinder
 
 class TestService(name: String?) : IntentService(name) {
 
+
+    /**
+     * всегда отрабатывает в другом потоке
+     */
     override fun onHandleIntent(intent: Intent?) {
-        TODO("Not yet implemented")
+
     }
+    /**
+     * Если служба уже запущена, ты не можешь создать еще один instance service
+     * Все команды, которые передаются, складываются в очередь и будут отрабатывать последовательно друг за другом.
+        *
+     */
+
     /**
      * Все службы наследуются от Service.
     Но есть IntentService, который также наследуется от Service
