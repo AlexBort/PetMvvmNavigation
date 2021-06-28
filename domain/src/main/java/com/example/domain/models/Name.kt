6545231785095a8fe7ed3f5dc70ffd8cc01357d5
Oleksandr_Bortnyk <alex.bortnik.it@gmailcom>
2021-06-28 18:06:40 +0300

    added necessary models to domain module

---
+++ domain/src/main/java/com/example/domain/models/Name.kt
@@ -0,0 +1,5 @@
+package com.example.domain.models
+
+data class Name(val title: String, val first: String, val last: String) {
+
+}
---
+++ domain/src/main/java/com/example/domain/models/User.kt
@@ -0,0 +1,8 @@
+package com.example.domain.models
+
+import java.util.*
+
+data class User(var name: Name, val email: String, var dateRegister: String, var picture: Picture
+) {
+
+}
