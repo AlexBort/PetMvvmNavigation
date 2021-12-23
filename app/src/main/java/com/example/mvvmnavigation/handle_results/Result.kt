42628a6a14f214a2863d95505ccd83bc237219c9
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2021-12-24 00:33:04 +0200

    added extension fun for getting success result

--- app/src/main/java/com/example/mvvmnavigation/handle_results/Result.kt
+++ app/src/main/java/com/example/mvvmnavigation/handle_results/Result.kt
@@ -7,6 +7,13 @@ sealed class Result<T>
 
 class ProgressResult<T> : Result<T>()
 
-class SuccessResult<T>(data: T) : Result<T>()
+class SuccessResult<T>(val data: T) : Result<T>()
 
 class ErrorResult<T>(error: Exception) : Result<T>()
+
+// adding extension function for defining success result:
+fun <T> Result<T>?.takeSuccess(): T? {
+    return if (this is SuccessResult) {
+        this.data
+    } else null
+}
