15a5a7ebd7b2e3b2e40b49ce821669a6c900c01f
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2021-12-27 18:02:34 +0200

    some work

--- app/src/main/java/com/example/mvvmnavigation/KtListViewModel.kt
+++ app/src/main/java/com/example/mvvmnavigation/KtListViewModel.kt
@@ -1,45 +1,35 @@
 package com.example.mvvmnavigation
 
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.OnLifecycleEvent
 import com.example.mvvmnavigation.api.ApiConnector
 import com.example.mvvmnavigation.api.ManResponse
 import com.example.mvvmnavigation.api.ResponseCallback
+import com.example.mvvmnavigation.handle_results.ErrorResult
+import com.example.mvvmnavigation.handle_results.ProgressResult
+import com.example.mvvmnavigation.handle_results.SuccessResult
 import com.example.mvvmnavigation.models.User
+import java.lang.Exception
 
 class KtListViewModel : BaseViewModel() {
 
     private val mtDataListString: MutableLiveResult<List<String>> = MutableLiveResult()
     private val mtDataString: MutableLiveResult<String> = MutableLiveResult()
-    private val mtDataUser: MutableLiveResult<List<User>> = MutableLiveResult()
+    private val mtDataUser: MutableLiveResult<List<User>> = MutableLiveResult(ProgressResult())
 
     val dataListString: LiveResult<List<String>> = mtDataListString
     val dataString: LiveResult<String> = mtDataString
     val dataListUser: LiveResult<List<User>> = mtDataUser
 
-//    fun getDataString(): MutableLiveData<String?>? {
-//        return mtDataString
-//    }
-
-//    fun getUsersList(): MutableLiveData<List<User?>?>? {
-//        return users
-//    }
-
-//    fun getData(): LiveData<List<String?>?>? {
-//        return data
-//    }
-
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onViewStarted() {
-        ApiConnector().sendRequestInfoList(object : ResponseCallback<ManResponse>{
+        ApiConnector().sendRequestInfoList(object : ResponseCallback<ManResponse> {
             override fun onSuccess(response: ManResponse) {
-//                mtDataUser.value = response.results
+                mtDataUser.value = SuccessResult(response.results)
             }
 
             override fun onError(throwable: Throwable) {
-
+                mtDataUser.value = ErrorResult(throwable as Exception)
             }
         }
         )
