32efee5bf979c21b5a56d03e5062f41a38219b13
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2022-08-22 16:57:11 +0300

    step 1 (from https://www.youtube.com/watch?v=1dOsef2ZzQ8&list=PL0SwNXKJbuNkYFUda5rlA-odAVyWItRCP&index=2)

---
+++ app/src/main/java/com/example/mvvmnavigation/for_dagger/Computer.kt
@@ -0,0 +1,11 @@
+package com.example.mvvmnavigation.for_dagger
+
+class Processor
+class Motherboard
+class RAM
+
+data class Computer(
+    val processor: Processor,
+    val motherboard: Motherboard,
+    val ram: RAM
+)
