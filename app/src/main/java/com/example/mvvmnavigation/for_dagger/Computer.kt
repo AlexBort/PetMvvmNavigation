f4de33807f5102de94b05563e1e8e1303d70fc2a
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2022-08-22 16:58:36 +0300

    step 1 (from https://www.youtube.com/watch?v=1dOsef2ZzQ8&list=PL0SwNXKJbuNkYFUda5rlA-odAVyWItRCP&index=2)

--- app/src/main/java/com/example/mvvmnavigation/for_dagger/Computer.kt
+++ app/src/main/java/com/example/mvvmnavigation/for_dagger/Computer.kt
@@ -1,8 +1,12 @@
 package com.example.mvvmnavigation.for_dagger
 
-class Processor
-class Motherboard
-class RAM
+class Processor {   override fun toString() = "A2021"}
+class Motherboard{
+    override fun toString() = "X7 3000"
+}
+class RAM{
+    override fun toString() = "16 GB"
+}
 
 data class Computer(
     val processor: Processor,
