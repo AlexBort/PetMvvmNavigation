0bea2155768eab341518a07058bdd2c53266e188
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2022-08-23 16:12:05 +0300

    implementation of dagger - Modules, typesOfInject, delivery methods: step 0 - https://www.youtube.com/watch?v=xif-1cnSHxs&list=PL0SwNXKJbuNkYFUda5rlA-odAVyWItRCP&index=3

--- app/src/main/java/com/example/mvvmnavigation/MainActivity.kt
+++ app/src/main/java/com/example/mvvmnavigation/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
             layoutInflater
         )
         setContentView(mainBinding!!.root)
-
 //        val computer: Computer = appComponent.computer()
         /**
          * instead of init (above commented code) we will do it another way
